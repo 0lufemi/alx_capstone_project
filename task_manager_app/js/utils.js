570fd801194134547fbf0ea5bafc09a686b10694
Olufemi Odunlami <femi9naija@gmail.com>
2023-11-26 22:30:29 +0100

    added reset feature

--- task_manager_app/js/utils.js
+++ task_manager_app/js/utils.js
@@ -2,6 +2,7 @@ const activityInput = document.querySelector('#activity-field');
 const dateInput = document.querySelector('#date-field');
 const timeInput = document.querySelector('#time-field');
 
+// Validate task forms
 const validateForm = () => {
     const inputs = document.querySelectorAll("input");
 
@@ -18,4 +19,26 @@ const validateForm = () => {
     }
 };
 
-export { validateForm };
+// Return task information
+const getActivityFields = () => {
+    const activity = activityInput.value;
+    const date = dateInput.value;
+    const time = timeInput.value;
+
+    return {activity, date, time}
+}
+
+// Reset form fields after adding task
+const reset = () => {
+    activityInput.value = '';
+    dateInput.value = '';
+    timeInput.value = '';
+}
+
+// const htmlElementCreator = (name, element, attr, attrName) => {
+//     name = document.createElement(element);
+//     name.setAttribute(attr, attrName);
+// }
+
+
+export { validateForm, getActivityFields, reset };
