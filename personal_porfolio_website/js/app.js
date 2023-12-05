85e592956e2df41656de2216ee56348d212ae402
Olufemi Odunlami <femi9naija@gmail.com>
2023-12-05 23:24:54 +0100

    Added JS

---
+++ personal_porfolio_website/js/app.js
@@ -0,0 +1,5 @@
+const section = document.querySelectorAll('.section');
+const controls = document.querySelectorAll('.controls');
+const control = document.querySelectorAll('.control');
+const bodySection = document.querySelectorAll('.main-content');
+
